fc9e86519b83f74b9faa12581677c1509ab2d054
Alejandro Mercado <pamercadom@hotmail.com>
2018-11-25 22:50:40 -0400

    Funcionamiento login

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() { }
+
+  public getRecursoLista(recurso: string): any[] {
+    let lista: any[] = JSON.parse(localStorage.getItem(recurso));
+    if (!lista) {
+      this.initRecurso(recurso);
+      lista = [];
+    }
+    return lista;
+  }
+
+  public getRecurso(recurso: string): any {
+    let respuesta: any = JSON.parse(localStorage.getItem(recurso));
+    return respuesta;
+  }
+
+  public initRecurso(recurso: string): void {
+    localStorage.setItem(recurso, JSON.stringify([]));
+  }
+
+  public postRecursoLista(recurso: string, data: any): void {
+
+    let lista: any[] = JSON.parse(localStorage.getItem(recurso));
+    lista.push(data);
+    localStorage.setItem(recurso, JSON.stringify(lista));
+  }
+
+  public postRecurso(recurso: string, data: any): void {
+    localStorage.setItem(recurso, JSON.stringify(data));
+  }
+
+}
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from '../model/usuario';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private usuarioRecurso: string = 'usuarios';
+  private usuarioSesionRecurso: string = 'usuariosesion';
+
+  constructor(private apiService: ApiService) {
+
+  }
+
+  public ingresarJuego(usuario: string): Usuario {
+    let usuarios = this.apiService.getRecursoLista(this.usuarioRecurso);
+    let usuarioNuevo = this.buscarUsuario(usuario, usuarios);
+    if (!usuarioNuevo) {
+      usuarioNuevo = new Usuario();
+      usuarioNuevo.idUsuario = usuarios.length + 1;
+      usuarioNuevo.usuario = usuario;
+      this.apiService.postRecursoLista(this.usuarioRecurso, usuarioNuevo);
+    }
+    this.apiService.postRecurso(this.usuarioSesionRecurso, usuarioNuevo);
+    return usuarioNuevo;
+  }
+
+  public buscarUsuario(usuario: string, listaUsuarios: Usuario[]): Usuario {
+    let respuesta: Usuario = null;
+    for (var i = 0; i < listaUsuarios.length; i++) {
+      if (listaUsuarios[i].usuario == usuario) {
+        respuesta = listaUsuarios[i];
+      }
+    }
+    return respuesta;
+  }
+
+  public getUsuarioActual(): Usuario {
+    return this.apiService.getRecurso(this.usuarioSesionRecurso);
+  }
+
 }
